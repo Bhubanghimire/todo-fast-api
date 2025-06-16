fdd7386597ac1a4087652f382ef75c0e13fcf934
bhuban <bhuban@nsdai.me>
2025-06-16 11:32:28 +0545

    jwt complete

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    JWT_SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = Path(__file__).resolve().parent / ".env"
         env_file_encoding = "utf-8"
+
 
 settings = Settings()
